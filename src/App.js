ffd67ff63f4d4d3d91592147ba0e01e115faa7e8
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-01-07 14:39:52 -0600

    imported BrowserRouter and added routes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/navbar/Navbar';
+
 
 function App() {
   return (
     <div>
-    
+      <Router>
+          <Navbar />
+            <Routes>
+            <Route path="/home" element={} />
+            <Route path="/home" element={} />
+            <Route path="/home" element={} />
+            <Route path="/home" element={} />
+          </Routes> 
+      </Router>
+        
         
         
       
