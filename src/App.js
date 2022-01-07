10f0ca8e720fe1233525453bd6d99f6f8b3c5112
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-01-07 15:31:28 -0600

    added routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/navbar/Navbar';
+import Home from './components/home/Home'
+import About from './components/about/About'
+import Project from './components/projects/Project'
+import Contact from './components/projects/Project'
+
 
 
 function App() {
@@ -11,10 +16,10 @@ function App() {
       <Router>
           <Navbar />
             <Routes>
-            <Route path="/" element={} />
-            <Route path="/about" element={} />
-            <Route path="/projects" element={} />
-            <Route path="/contact" element={} />
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Project />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes> 
       </Router>
         
