9042fcd5b74bcfb4106a59482d168b2a1156bea0
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-01-07 14:02:39 -0600

    importing and building Link for routing in Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
     return (
         <div>
-            
+            <Link></Link>
+            <Link></Link>
+            <Link></Link>
+            <Link></Link>
         </div>
     )
 }
