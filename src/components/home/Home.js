257cd7f00bf445dfda85f62d4844cc116bcdecea
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-01-08 14:16:16 -0600

    fixed home page bug

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,7 +3,8 @@ import React from "react"
 const Home = () => {
     return (
         <div>
-
+            <h1>Software Engineer</h1>
+            gjdlkgdlfgkjdlfj
         </div>
     )
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <nav id="nav">
             <div>
-                <Link id="link" to="/home">HOME</Link>
+                <Link id="link" to="/">HOME</Link>
                 <Link id="link" to="/about">ABOUT</Link>
                 <Link id="link" to="/projects">PROJECTS</Link>
                 <Link id="link" to="/contact">CONTACT</Link>
