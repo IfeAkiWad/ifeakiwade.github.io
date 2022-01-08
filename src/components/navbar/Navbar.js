c10c0953ec9ed44a8b1c8fcd96fa877cc4834265
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-01-08 14:08:24 -0600

    finished nav bar CSS

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom"
 
 const Navbar = () => {
     return (
-        <div>
-            <Link id="link" to="/home">HOME</Link>
-            <Link id="link" to="/about">ABOUT</Link>
-            <Link id="link" to="/projects">PROJECTS</Link>
-            <Link id="link" to="/contact">CONTACT</Link>
-        </div>
+        <nav id="nav">
+            <div>
+                <Link id="link" to="/home">HOME</Link>
+                <Link id="link" to="/about">ABOUT</Link>
+                <Link id="link" to="/projects">PROJECTS</Link>
+                <Link id="link" to="/contact">CONTACT</Link>
+            </div>
+        </nav>    
     )
 }
 export default Navbar
