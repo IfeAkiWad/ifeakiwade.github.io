ace83df4b05682f08f73e76bbe14410ae258fd06
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-01-08 17:13:17 -0600

    beginning to build Project.js

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -3,7 +3,9 @@ import React from "react"
 const Project = () => {
     return (
         <div>
-
+            <h1>My Projects</h1>
+            <h2>These are projects that I've built.</h2>
+            <h3>They are projects that I am always looking to improve.</h3>
         </div>
     )
 }
