4a17f7784858d9546603c581a5f1cbf07c0eb15c
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-01-08 15:34:31 -0600

    finished about me page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,7 +3,8 @@ import React from "react"
 const About = () => {
     return (
         <div>
-            YO YO YO
+            <h1>About Me</h1>
+            <p>I'm a recent Flatiron school graduate with a background in Psychology. I graduated from California Baptist University with a Bachelor's of Arts in Psychology before making my transition into Software engineering. I am currently available for an entry-level/junior frontend developer role, or apprenticeship.</p>
 
         </div>
     )
