e3655d10800f04a1736b9f410f16362c09fac5a7
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-01-08 17:02:49 -0600

    minor changes in Home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -7,8 +7,8 @@ const Home = () => {
             <p>Welcome to my website! My name is</p>
             <h1>Ifeoluwa Akinremi-Wade.</h1>
             <h1>I'm a Software Engineer.</h1>
-            <p>My very first coding language is Ruby, but I've fallen in love with Javascript. I am a React developer with experience in Ruby on Rails. </p>
-            <br /><br /><br /><br />
+            <p>My very first coding language was Ruby, but I've fallen in love with Javascript. I am a growing React developer with experience in Ruby on Rails. </p>
+            <br /><br />
             <About />
             
         </div>
