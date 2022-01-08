877c495fb974b1eb9c1208b7bac550df44d1c01f
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-01-08 14:55:58 -0600

    finished home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,8 +3,10 @@ import React from "react"
 const Home = () => {
     return (
         <div>
-            <h1>Software Engineer</h1>
-            gjdlkgdlfgkjdlfj
+            <p>Welcome to my website! My name is</p>
+            <h1>Ifeoluwa Akinremi-Wade.</h1>
+            <h1>I'm a Software Engineer.</h1>
+            <p>My very first coding language is Ruby, but I've fallen in love with Javascript. I am a React developer with experience in Ruby on Rails. </p>
         </div>
     )
 }
