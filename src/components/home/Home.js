2d23ee1b619e0db02bf9d5d314a2fc6e7bee24c1
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-01-08 17:07:47 -0600

    imported other components in to Home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,7 @@
 import React from "react"
 import About from "../about/About";
+import Project from "../projects/Project";
+import Contact from "../contact/Contact"
 
 const Home = () => {
     return (
@@ -10,6 +12,10 @@ const Home = () => {
             <p>My very first coding language was Ruby, but I've fallen in love with Javascript. I am a growing React developer with experience in Ruby on Rails. </p>
             <br /><br />
             <About />
+            <br /><br />
+            <Project />
+            <br /><br />
+            <Contact />
             
         </div>
     )
