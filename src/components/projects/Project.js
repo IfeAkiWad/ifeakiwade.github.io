45916a1f34db3b084154eab77af7a252267c5c10
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-01-09 15:39:54 -0600

    working on adding projects

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -4,8 +4,8 @@ const Project = () => {
     return (
         <div>
             <h1>My Projects</h1>
-            <h2>These are projects that I've built.</h2>
-            <h3>They are projects that I am always looking to improve.</h3>
+            <h2>Projects that I've built.</h2>
+            <h3>I am always looking to improve them.</h3>
         </div>
     )
 }
