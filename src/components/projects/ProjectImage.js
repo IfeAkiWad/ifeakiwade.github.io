f009a67c400215f10541f17a6306a77ed8a93e4e
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-01-09 15:59:30 -0600

    importing project image files into ProjectImage.js

---
+++ src/components/projects/ProjectImage.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Project from "./Project"
+import PortfolioCommonHealth from "../PortfolioCommonHealth.jpg"
+import PortfolioDayCare from "../PortfolioDayCare.jpg"
+import PortfolioProjectManager from "../PortfolioProjectManager.jpg"
+
+const ProjectImage = () => {
+
+
+
+    return(
+        <div>
+            <Project />
+        </div>
+    )
+}
+export default ProjectImage
