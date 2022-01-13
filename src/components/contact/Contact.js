e95b8ea3c4718bf3281fc9d2e3f94059f33b12cd
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-01-13 12:15:48 -0600

    building Contact.js

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -3,7 +3,9 @@ import React from "react"
 const Contact = () => {
     return (
         <div>
-
+            <h1>Let's Make Contact!</h1>
+            <p>My inbox is open to opportunities!</p>
+            <h1 className="email-icon"> 📨 </h1>
         </div>
     )
 }
