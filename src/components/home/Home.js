363b0bdcee22cfde78c39660e9447d5fa4c9d079
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-01-16 23:06:40 -0600

    designing with CSS in components & added footer

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,7 +6,7 @@ import Contact from "../contact/Contact"
 const Home = () => {
     return (
         <div id="home">
-            <p>Welcome to my website! My name is</p>
+            <p className="welcome">Welcome to my website! My name is</p>
             <h1>Ifeoluwa Akinremi-Wade.</h1>
             <h1>I'm a Software Engineer.</h1>
             <p>My very first coding language was Ruby, but I've fallen in love with Javascript. I am a growing React developer with experience in Ruby on Rails. </p>
