2e692d30dc2c75668d7a093387bb801faf7b0286
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-01-17 10:27:19 -0600

    added email button instead

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -6,7 +6,9 @@ const Contact = () => {
             <h1>Let's Make Contact!</h1>
             <p>My inbox is open to opportunities!</p>
             <a href= "mailto:ifeoluwa.akinremi.wade@gmail.com">
-                <h1 className="email-icon"> 📨 </h1>
+                <button className="email-btn">
+                    <h1 className="email-text"> Reach Out! </h1>
+                </button>
             </a>
         </div>
     )
