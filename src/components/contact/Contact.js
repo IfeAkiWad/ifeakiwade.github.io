2dedc787fc0d6c281d8c20fb59d7cc136449ead0
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-01-17 12:19:37 -0600

    general HTML/CSS edit

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -6,11 +6,11 @@ const Contact = () => {
             <h1 className="contact-header">Let's Make Contact!</h1>
             <p>My inbox is open to opportunities!</p>
             <div>
-                <button className="email-btn">
-                    <a href= "mailto:ifeoluwa.akinremi.wade@gmail.com" rel="noopener noreferrer" target="_blank">
-                        <h1 className="email-text">Hello</h1>
+                {/* <button className="email-btn"> */}
+                    <a className="email" href= "mailto:ifeoluwa.akinremi.wade@gmail.com" rel="noopener noreferrer" target="_blank">
+                        <h1 className="email-text">How Ya Doin'?</h1>
                     </a>
-                </button>
+                {/* </button> */}
             </div>
         </div>
     )
