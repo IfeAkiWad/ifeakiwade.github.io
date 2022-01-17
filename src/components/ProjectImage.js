74a172d91f6f326600b44131d3e89ac17e0da8df
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-01-16 21:41:28 -0600

    project image html/css styling

--- src/components/ProjectImage.js
+++ src/components/ProjectImage.js
@@ -12,7 +12,7 @@ const ProjectImage = () => {
                 </a>
                     
                 <div className="container">
-                    <p id="title-day-care">CommonHealth: Ruby on Rails </p>
+                    <p id="title-day-care"><u><b>Sunny Day Care:</b></u> React/Redux client-side and Ruby on Rails server-side.<br /><br />A React application that maintains a database for a daycare and enrolled children.  </p>
                 </div>
             </div>
                 
@@ -21,7 +21,7 @@ const ProjectImage = () => {
                     <img className="img-proj-manager" src={PortfolioProjectManager} alt="project manager project"/>
                 </a>     
                 <div className="container">
-                    <p id="title-proj-manager">CommonHealth: Ruby on Rails </p>
+                    <p id="title-proj-manager"><u><b>Project Manager:</b></u> Javascript client-side and Ruby on Rails server-side.<br /><br />Javascript single-page application that creates and manages project tasks.</p>
                 </div>
             </div>    
             <div className="polaroid" >
@@ -29,7 +29,7 @@ const ProjectImage = () => {
                     <img className="img-common-health" src={PortfolioCommonHealth} alt="common health project"/>
                 </a>
                 <div className="container">
-                    <p id="title-common-health">CommonHealth: Ruby on Rails </p>
+                    <p id="title-common-health"><u><b>Common Health:</b></u> Ruby on Rails with full MVC Structure.<br /><br /> Ruby on Rails Health-based web application that connects black doctors of various specialties to black patients.</p>
                 </div>
                     
            </div>
