2384937ed0378baec4ea9ee734e88479f40d842c
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-01-17 10:07:04 -0600

    theres an issue with importing image

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,8 +1,10 @@
 import React from "react"
+// import KentePortfolio from "../KentePortfolio.jpeg"
 
 const About = () => {
     return (
         <div id="about">
+             {/* <img className='kente-portfolio' src={KentePortfolio} alt='kente-cloth'/> */}
             <h1 className="about-me">About Me</h1>
             <p>I'm a recent Flatiron school graduate with a background in Psychology. I graduated from California Baptist University with a Bachelor's of Arts in Psychology before making my transition into Software engineering. I am currently available for an entry-level/junior frontend developer role, or apprenticeship.</p>
 
