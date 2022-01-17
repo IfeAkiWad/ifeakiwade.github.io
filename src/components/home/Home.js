6b07aea851fe234255dc308bab937bf8e071dec8
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-01-17 10:57:47 -0600

    divided components by <section>

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -7,16 +7,24 @@ import Footer from "../footer/Footer";
 const Home = () => {
     return (
         <div id="home">
-            <p className="welcome">Welcome to my website! My name is</p>
-            <h1>Ifeoluwa Akinremi-Wade.</h1>
-            <h2>I'm a Software Engineer.</h2>
-            <p>My very first coding language was Ruby, but I've fallen in love with Javascript. I am a growing React developer with experience in Ruby on Rails. </p>
+            <section>
+                <p className="welcome">Welcome to my website! My name is</p>
+                <h1>Ifeoluwa Akinremi-Wade.</h1>
+                <h2>I'm a Software Engineer.</h2>
+                <p>My very first coding language was Ruby, but I've fallen in love with Javascript. I am a growing React developer with experience in Ruby on Rails. </p>
+            </section>
             <br /><br />
-            <About />
+            <section>
+                <About />
+            </section>
             <br /><br />
-            <Project />
+            <section>
+                <Project />
+            </section>
             <br /><br />
-            <Contact />
+            <section>
+                <Contact />
+            </section>
             <Footer />
             
         </div>
