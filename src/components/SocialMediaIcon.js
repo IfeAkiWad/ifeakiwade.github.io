388675f7b59e6b6ecb116f9301dca90a0ec888cd
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-01-18 17:52:51 -0600

    building social media icon component

---
+++ src/components/SocialMediaIcon.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import  ReactDOM  from  'react-dom';
+import {SocialMediaIconsReact} from 'social-media-icons-react';
+
+ReactDOM.render(<SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" icon="twitter" iconColor="rgba(255,255,255,1)" backgroundColor="rgba(26,166,233,1)" url="https://some-website.com/my-social-media-url" size="48" />,	document.getElementById('root'));
